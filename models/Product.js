75a478d9debf38071e4d01ccdf16d66e6429dec6
magic404 <jamesbox3@gmail.com>
2023-05-02 14:50:38 -0700

    tag.js complete

--- models/Product.js
+++ models/Product.js
@@ -34,7 +34,16 @@ Product.init(
       validate: {
         isNumeric:true,
       }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references:{
+        model:"category",
+        key:"id"
+      } 
     }
+    
+
   },
   {
     sequelize,
